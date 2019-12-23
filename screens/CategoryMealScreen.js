b7d970208eda258012b9d98b498a77521aefaa63
Maksym Shokin <maksym.sh@upplabs.com>
2019-12-23 16:18:25 +0200

    'dynamically-header-options'

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -2,16 +2,17 @@ import React from 'react';
 import {View, Text, StyleSheet, Button, Platform} from 'react-native';
 
 import {CATEGORIES} from '../data/dummy-data';
+import Colors from '../constants/Colors';
+import CategoriesScreen from './CategoriesScreen';
 
 const CategoryMealScreen = props => {
   const catId = props.navigation.getParam('categoryId');
 
   const catTitle = CATEGORIES.find(cat=> cat.id === catId);
-
   return (
     <View style={styles.screen}>
-      <Text>CategoryMeals</Text>
       <Text>{catTitle.title}</Text>
+      <Text>CategoryMeals</Text>
       <Button title='Go to Meals Screen' onPress={()=>{
         props.navigation.navigate({
           routeName: 'MealDetail'
@@ -19,6 +20,19 @@ const CategoryMealScreen = props => {
       }}/>
     </View>
   )
+};
+
+CategoryMealScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam('categoryId');
+  const catTitle = CATEGORIES.find(cat=> cat.id === catId);
+
+  return {
+    headerTitle: catTitle.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+  }
 };
 
 const styles = StyleSheet.create({
