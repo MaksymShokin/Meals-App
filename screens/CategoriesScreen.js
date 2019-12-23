be7b0c6bb1bd9cd3fc7ae84ff1edd9e8c3023a13
Maksym Shokin <maksym.sh@upplabs.com>
2019-12-23 15:20:48 +0200

    'set-header-options'

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, FlatList, TouchableOpacity, Platform} from 'react-native';
+
+import {CATEGORIES} from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = props => {
-  console.log(props)
-  return (
-    <View style={styles.screen}>
-      <Text>CategoriesScreen</Text>
-      <Button title='GO TO MEALS' onPress={()=>{
-        props.navigation.navigate({routeName: 'CategoryMeal'})
-      }}/>
-    </View>
-  )
+  const renderListItem = itemData => {
+    return (
+      <TouchableOpacity style={styles.listItem} onPress={()=>{
+        props.navigation.navigate({routeName: 'MealDetail'})
+      }}>
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  };
 
+  return (
+    <FlatList
+      data={CATEGORIES}
+      numColumns={2}
+      renderItem={renderListItem}
+    />
+  )
 };
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meals Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+};
+
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  listItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 
