e0d7b15a5254b0147cf5b0dc685611612664e617
Maksym Shokin <maksym.sh@upplabs.com>
2019-12-29 01:21:13 +0200

    'fav-meal-noText'

--- screens/FavouriteMealScreen.js
+++ screens/FavouriteMealScreen.js
@@ -3,13 +3,24 @@ import MealList from '../components/MealList';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../components/HeaderButtons';
 import {useSelector} from 'react-redux';
+import {View, Text, StyleSheet} from 'react-native';
+import Colors from '../constants/Colors';
 
 const FavouriteMealScreen = props => {
   const mealId = props.navigation.getParam('mealId');
   const availableMeals = useSelector(state => state.meals.favouriteMeals);
 
+  let mealsToDisplay = <MealList navigation={props.navigation} mealsToDisplay={availableMeals}/>;
+
+  if (!availableMeals.length) {
+    mealsToDisplay =
+      <View style={styles.screen}><Text style={styles.noMealsText}>No meals are selected as favourite</Text></View>
+  }
+
   return (
-    <MealList navigation={props.navigation} mealsToDisplay={availableMeals}/>
+    <React.Fragment>
+      {mealsToDisplay}
+    </React.Fragment>
   )
 };
 
@@ -18,10 +29,23 @@ FavouriteMealScreen.navigationOptions = navData => {
     headerTitle: 'Your favourite meals!',
     headerLeft: (
       <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-        <Item title='sideMenu' iconName='ios-menu' onPress={()=> navData.navigation.toggleDrawer()}/>
+        <Item title='sideMenu' iconName='ios-menu' onPress={() => navData.navigation.toggleDrawer()}/>
       </HeaderButtons>
     )
   }
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  noMealsText: {
+    fontSize: 22,
+    fontFamily: 'open-sans-bold',
+    color: Colors.primaryColor
+  }
+});
 
 export default FavouriteMealScreen;
